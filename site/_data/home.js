8d95ba135b5c1bdcc3c46bead2dff4c49d2a0176
Shishak <semenshishak@gmail.com>
2020-01-31 14:03:38 +0200

    Update home.js

--- site/_data/home.js
+++ site/_data/home.js
@@ -1,7 +1,7 @@
 var axios  = require('axios');
 var toJSON = require('xml2js').parseString;
 
-var url = process.env.MEDIUM_FEED ||'https://www.jpl.nasa.gov/multimedia/rss/news.xml';
+var url = process.env.MEDIUM_FEED ||'https://www.pipes.digital/feedpreview/1qW4ll9y?feed=https%3A%2F%2Fwww.nasa.gov%2Frss%2Fdyn%2Fbreaking_news.rss';
 
 module.exports = () => {
   return new Promise((resolve, reject) => {
